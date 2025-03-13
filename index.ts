bd04324061a06d9c1dbd83e744c10dee116b68a0
TNZtims <timotniellau@gmail.com>
2025-03-13 23:14:19 +0800

    initial index.js

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as People from './apis/people'
+import * as Projects from './apis/projects'
+import * as Todosets from './apis/todosets'
+import * as Todolists from './apis/todolists'
+import * as TodolistsGroups from './apis/todolistGroups'
+import * as Todos from './apis/todos'
+import * as Comments from './apis/comments'
+
+export {
+  People,
+  Projects,
+  Todosets,
+  Todolists,
+  TodolistsGroups,
+  Todos,
+  Comments
+}
